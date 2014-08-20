057cdb604cbfb2caf82c194d0a5c8335875dfb1b
Sean McGary <sean@seanmcgary.com>
2014-08-20 09:37:28 -0700

    Initial Commit

---
+++ Harvest/Harvest.m
@@ -0,0 +1,13 @@
+//
+//  Harvest.m
+//  Harvest
+//
+//  Created by Sean McGary on 8/20/14.
+//  Copyright (c) 2014 Sean McGary. All rights reserved.
+//
+
+#import "Harvest.h"
+
+@implementation Harvest
+
+@end
